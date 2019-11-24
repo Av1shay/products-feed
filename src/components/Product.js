910ebac33adb7f3ca7651d62b1d9cdfb12a35b5e
Avishay <avishay.guttman@gmail.com>
2019-11-24 19:49:48 +0200

    remove unnecessary comments

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
  
 class Product extends Component {
-    constructor(props) {
-        super(props);
-      }
-
-
+    
     render() {
         const { key, title, description, link, imageLink, price, salePrice } = this.props;
 
